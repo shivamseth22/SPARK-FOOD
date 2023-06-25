1235f96d68e4809807c06f586e5e38e07b3f4933
Shivam seth <shivamseth221104@gmail.com>
2023-06-25 22:48:15 +0530

    okk

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import {ImEqualizer2} from "react-icons/im"
 
 const Header = () => {
   return (
-    <div className="flex justify-between pt-10 bg-yellow-100 text-lg">
+    <div className="flex justify-between pt-10  text-lg sticky top-0 z-20 bg-white">
       <div>
         <h1 className="text-2xl font-bold">315 resturants</h1>
       </div>
@@ -12,7 +13,11 @@ const Header = () => {
         <h1>Rating</h1>
         <h1>Cost: Low To High</h1>
         <h1>Cost: High To Low</h1>
+        <div className="flex items-center gap-1">
+        
         <h1>Filters</h1>
+        <ImEqualizer2/>
+        </div>
       </div>
     </div>
   );
