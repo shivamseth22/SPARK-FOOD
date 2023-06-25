5eaaf044e970400259ff3c82c76a52163f869aab
Shivam seth <shivamseth221104@gmail.com>
2023-06-25 22:47:04 +0530

    okk

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,38 @@
-import './App.css';
+import "./App.css";
 import Navbar from "./Component/Navbar";
-import Header from './Component/Header';
-import Body from './Component/Body';
-import Footer from './Component/Footer';
+import Header from "./Component/Header";
+import Body from "./Component/Body";
+import Footer from "./Component/Footer";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import Error from "./Component/Error";
+import ResturantMenu from "./Component/ResturantMenu";
 
 function App() {
-
-
   return (
     <>
-     <Navbar/>
-     <div>
-     <Header/>
-     <Body/>
-     </div>
-     <Footer/>
+      <Navbar />
+      <Header />
+      <Outlet />
+      <Footer />
     </>
-  )
+  );
 }
+export const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/resturant/:id",
+        element:<ResturantMenu/>,
+      },
+    ],
+    errorElement: <Error />,
+  },
+]);
 
-export default App
+export default App;
