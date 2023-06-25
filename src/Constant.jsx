648a3850e5791169c9d83e766fcf9267b201ca01
Shivam seth <shivamseth221104@gmail.com>
2023-06-25 22:47:42 +0530

    ok

--- src/Constant.jsx
+++ src/Constant.jsx
@@ -1,1 +1,7 @@
 export const API = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&page_type=DESKTOP_WEB_LISTING";
+
+
+export const Res_Img ="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+
+export const ResMenu_API ="https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&menuId=";
