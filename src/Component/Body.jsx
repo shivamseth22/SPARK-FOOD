520e500d390a07e76ac522039ba699d85aba2760
Shivam seth <shivamseth221104@gmail.com>
2023-06-25 22:47:58 +0530

    okk

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { API } from "../Constant";
+import ResturantList from "./ResturantList";
 
 const Body = () => {
-  const [apiData, setApiData] = useState("shivam");
-  console.log(apiData);
+  
+  const [apiData, setApiData] = useState(null);
+  // console.log(apiData);
 
   const getData = async () => {
     const data = await fetch(API);
     const json = await data.json();
     setApiData(json?.data?.cards[2]?.data?.data?.cards);
-    console.log(json?.data?.cards[2]?.data?.data?.cards);
   };
   useEffect(() => {
     getData();
   }, []);
 
-  return (
-    <div>
-      {/* {apiData &&
-        apiData.map((item) => (
-        <h1>{item.data.name}</h1>)
-        )} */}
-    </div>
-  );
+  return (<div className="flex flex-wrap justify-between mt-10">
+  {
+    apiData && 
+    apiData?.map((item) => <ResturantList key={item?.data?.id} res={item}/>
+    
+    )}
+    </div>);
+  // return <>{apiData && apiData?.map((res) => console.log(res))}</>;
 };
 
 export default Body;
