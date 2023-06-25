b66fea69adedc74cfe1e8a21e217f0e5c661a234
Shivam seth <shivamseth221104@gmail.com>
2023-06-26 01:50:43 +0530

    okk

--- src/Component/ResturantMenu.jsx
+++ src/Component/ResturantMenu.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
-import { ResMenu_API } from '../Constant';
-import { Res_Img } from '../Constant';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {  ResMenu_API } from "../Constant";
+import { Res_Img } from "../Constant";
 
 const ResturantMenu = () => {
-    const {id} = useParams();
-    console.log(id);
-    const [menu , setMenu] = useState({})
+  const { id } = useParams();
+  console.log(id);
+  const [menu, setMenu] = useState({});
 
-    const getResturantMenu = async()=>{
-        const data = await fetch(ResMenu_API);
-        const json = await data.json();
-        console.log(json.data.cards[3].data.data);
-        setMenu(json.data.cards[3].data.data)
-    }
+  const getResturantMenu = async () => {
+    const data = await fetch(ResMenu_API);
+    const json = await data.json();
+    // console.log(json.data.cards[3].data.data);
+    setMenu(json?.data?.cards);
+    // console.log(json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card);
+  };
 
-    useEffect(()=>{
-        getResturantMenu()
-    },[])
+  useEffect(() => {
+    getResturantMenu();
+  }, []);
 
   return (
     <>
-        <h1>Resturant id:123</h1>
-        <h1>{menu.name}</h1>
-        <img src={Res_Img + menu.cloudinaryImageId} alt="" />
-        <h1>{menu.area}</h1>
-        <h1>{menu.city}</h1>
-        <h1>{menu.avgRating} star</h1>
-        <h1>{menu.costForTwo}</h1>
-
+      <h1>Resturant id:123</h1>
+      <h1>{menu.name}</h1>
+      <img src={Res_Img + menu.cloudinaryImageId} alt="" />
+      <h1>{menu.area}</h1>
+      <h1>{menu.city}</h1>
+      <h1>{menu.avgRating} star</h1>
+      <h1>{menu.costForTwo}</h1>
     </>
-  )
-}
+  );
+};
 
-export default ResturantMenu; 
+export default ResturantMenu;
