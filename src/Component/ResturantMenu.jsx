b2807715b4664e3b9d69759335db870f57f43d94
Shivam seth <shivamseth221104@gmail.com>
2023-06-26 16:05:44 +0530

    ok

--- src/Component/ResturantMenu.jsx
+++ src/Component/ResturantMenu.jsx
@@ -22,15 +22,17 @@ const ResturantMenu = () => {
   }, []);
 
   return (
-    <div className="max-w-[700px] min-w-[700px] m-auto">
+    <div className="max-w-[800px] min-w-[800px] m-auto mt-10">
       {
-        console.log(menu[0]?.card?.card?.info)
+        /* console.log(menu[0]?.card?.card?.info) */
+        console.log(menu[1]?.card?.card?.gridElements?.infoWithStyle?.offers)
 
         /* {menu &&
         Object.values(menu[0])?.map((res) => (
           <div>{console.log(res?.card?.info)}</div>
         ))} */
       }
+      <div>
       <div className="flex justify-between">
         <div>
           <h1 className="font-bold text-lg">
@@ -39,7 +41,7 @@ const ResturantMenu = () => {
           <h1 className="flex text-sm">
             {menu[0]?.card?.card?.info?.cuisines}
           </h1>
-          <div className="flex text-sm">
+          <div className="flex text-sm gap-1">
             <h1>{menu[0]?.card?.card?.info?.areaName}</h1>
             <h1>{menu[0]?.card?.card?.info?.sla?.lastMileTravelString}</h1>
           </div>
@@ -49,10 +51,35 @@ const ResturantMenu = () => {
             <AiOutlineStar />
             <h1>{menu[0]?.card?.card?.info?.avgRatingString}</h1>
           </div>
-          <h1 className="text-xs">{menu[0]?.card?.card?.info?.totalRatingsString}</h1>
+          <h1 className="text-xs">
+            {menu[0]?.card?.card?.info?.totalRatingsString}
+          </h1>
         </div>
       </div>
       {/* totalRatingsString */}
+    </div>
+    <div className="flex my-5 gap-10 ">
+      <h1>{menu[0]?.card?.card?.info?.sla?.slaString}</h1>
+      <h1>{menu[0]?.card?.card?.info?.costForTwoMessage}</h1>
+    </div>
+    <div>
+    <div className="flex  justify-between ">
+    {(menu[1]?.card?.card?.gridElements?.infoWithStyle?.offers)?.map((inner)=>(
+      <div className="bg-red-200 border-solid border-2 border-black min-w-200px">
+      <h1 className="text-sm">{inner.info.header}</h1>
+      <div className="text-xs flex divide-x-2">
+      <h1>{inner.info.couponCode}</h1>
+      <h1>{inner.info.description}</h1>
+      </div>
+      </div>
+    ))}
+    </div>
+    {/* <h1>{menu[0]?.card?.card?.info?.aggregatedDiscountInfo?.shortDescriptionList[1]?.meta}</h1> */}
+    </div>
+
+    <div>
+
+    </div>
     </div>
   );
 };
