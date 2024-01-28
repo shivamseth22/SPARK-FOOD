5440d3bd849a4f60c2cb505149a938ffae1fe2fa
shivamsetha2d <153591512+shivamsetha2d@users.noreply.github.com>
2024-01-29 01:33:20 +0530

    ok

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
 
+
+const mydata = [
+  {
+    name:"shivam",
+    age:"21",
+    id:"1"
+  },
+  
+  {
+    name:"ram",
+    age:"25",
+    id:"2"
+  },
+  {
+    name:"shyam",
+    age:"28",
+    id:"3"
+  },
+]
+const MYRESTRO = ({mydata}) => {
+  return (
+    <div >
+      <h1>{mydata.name}</h1>
+      <h1>{mydata.age}</h1>
+      <h1>{mydata.id}</h1>
+    </div>
+  )
+}
+
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className=' '>
+    <h1>hello</h1>
+      <MYRESTRO mydata={mydata[0]}/>
+      <MYRESTRO mydata={mydata[1]}/>
+      <MYRESTRO mydata={mydata[2]}/>
+    </div>
   )
 }
 
